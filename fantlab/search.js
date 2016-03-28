2efefe3fed8d8118772a3502ba42375e63a67f36
Andrey Dolganov <andrey@dolganov.xyz>
2016-03-28 19:41:49 +0300

    Убирает кнопку у ссылки "все результаты >"

--- fantlab/search.js
+++ fantlab/search.js
@@ -1,7 +1,8 @@
 $(function () {
     new Clipboard("#copy-id");
     $(".search-results a")
-        .not(":has(img)")
+        .not("a:has(img)")
+        .not(".more-link-block a")
         .filter(function(index) { return this.text.trim().length > 0; })
         .after(function() {
             return " <button id='copy-id' data-clipboard-text='" + /\d+/.exec(this.href)[0]  + "'>id</button>";
