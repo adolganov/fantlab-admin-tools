6de029dde94766335e19e776a44da3b337391a7d
Andrey Dolganov <andrey@dolganov.xyz>
2016-03-30 21:49:06 +0300

    Замена идентификации кнопок по ид на класс

--- fantlab/search.js
+++ fantlab/search.js
@@ -1,10 +1,11 @@
 $(function () {
-    new Clipboard("#copy-id");
     $(".search-results a")
         .not("a:has(img)")
         .not(".more-link-block a")
         .filter(function(index) { return this.text.trim().length > 0; })
         .after(function() {
-            return " <button id='copy-id' data-clipboard-text='" + /\d+/.exec(this.href)[0]  + "'>id</button>";
+            return " <button class='id-btn' data-clipboard-text='" + /\d+/.exec(this.href)[0]  + "'>id</button>";
         });
+
+    new Clipboard(".id-btn");
 });
